36659802ce0ce855b4acf230ef5112515446e07e
morningstarwang <morningstarwang@outlook.com>
2021-02-15 11:37:00 +0800

    20210215

---
+++ leetcode/240.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        n = len(matrix)
+        if n == 0:
+            return False
+        m = len(matrix[0])
+        row = 0
+        column = m - 1
+        while row < n and column >= 0:
+            if matrix[row][column] == target:
+                return True
+            if matrix[row][column] > target:
+                column -= 1
+            elif matrix[row][column] < target:
+                row += 1
+        return False
