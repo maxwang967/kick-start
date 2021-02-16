2464f1294abc29eaadf7b2f6693cca6e411ff59e
morningstarwang <morningstarwang@outlook.com>
2021-02-16 17:03:03 +0800

    20210216

---
+++ leetcode/LCOF08.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+# class TreeLinkNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+#         self.next = None
+class Solution:
+    def GetNext(self, pNode):
+        # write code here
+        if pNode.right:#有右子树
+            p=pNode.right
+            while p.left:
+                p=p.left
+            return p
+        while pNode.next:#无右子树，则找第一个当前节点是父节点左孩子的节点
+            if(pNode.next.left==pNode):
+                return pNode.next
+            pNode = pNode.next#沿着父节点向上遍历
+        return  #到了根节点仍没找到，则返回空   
