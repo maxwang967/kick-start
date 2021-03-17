aa0af749c330e951c5847d2cde408bb69b8ff7c2
morningstarwang <morningstarwang@outlook.com>
2021-03-17 21:34:04 +0800

    20210317

---
+++ leetcode/LCOF17.py
@@ -0,0 +1,12 @@
+class Solution:
+    def printNumbers(self, n: int) -> List[int]:
+        def dfs(x):
+            if x == n:
+                pass
+            for i in range(10):
+                pass
+        # return list(range(1, 10 ** n))
+        
+        result = []
+        self.start = n - 1
+        self.nine = 0
